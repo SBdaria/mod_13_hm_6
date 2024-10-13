20fb132631e3662169878e5a19cfd2bfaa31398c
SBdaria <168875873+SBdaria@users.noreply.github.com>
2024-10-13 19:00:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -24,18 +24,28 @@ class UserState(StatesGroup):
     growth = State()
     weight = State()
 
+
 @dp.message_handler(commands=['start'])
 async def start_message(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def start_message(message):
+    await message.answer('Я помогу вам рассчитать необходимую норму каллорий '
+                         'в соответствии с вашим возрастом, весом и ростом:)', reply_markup=kb)
+
 
 @dp.message_handler(text='Рассчитать')
 async def main_menu(message):
     await message.answer('Выберите опцию:', reply_markup=inkb)
 
+
 @dp.callback_query_handler(text='formulas')
 async def get_formulas(call):
     await call.message.answer('10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
     await call.answer()
+
 
 @dp.callback_query_handler(text='calories')
 async def set_age(call):
@@ -43,17 +53,20 @@ async def set_age(call):
     await call.answer()
     await UserState.age.set()
 
+
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
     await state.update_data(age=message.text)
     await message.answer('Введите свой рост:')
     await UserState.growth.set()
 
+
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
     await state.update_data(growth=message.text)
     await message.answer('Введите свой вес:')
     await UserState.weight.set()
+
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
@@ -63,9 +76,11 @@ async def send_calories(message, state):
     await message.answer(f'Ваша норма калорий {round(res, 2)}')
     await state.finish()
 
+
 @dp.message_handler()
 async def all_massages(message):
     await message.answer('Введите команду /start, чтобы начать общение.')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
